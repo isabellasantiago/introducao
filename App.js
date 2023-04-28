93e5e98deade6c1c49ee47eab74764c611d0e5dd
Isabella Santiago <isabella.-.bela@hotmail.com>
2023-04-28 20:35:57 -0300

    apostila 08

---
+++ App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [texto, setTexto] = useState('Oi');
+
+  return (
+    <View style={{
+      bordercolor: 'black',
+      borderWidth: 1,
+      width: '100%',
+      alignItems: 'center',
+      padding: 20,
+    }}>
+      <Text
+        style={{
+          width: '80%',
+          borderColor: "#CCC",
+          borderWidth: 1,
+          textAlign: 'center',
+          backgroundColor: "#DDD",
+          padding: 8,
+          borderRadius: 8,
+          marginBottom: 4
+        }}
+      >
+        {texto}
+      </Text>
+      <View style={{ width: '80%'}}>
+        <Button 
+          title="OK"
+          onPress={() => setTexto("Olá, Mundo!!!")}
+        />
+      </View>
+    </View>
+  );
+}
